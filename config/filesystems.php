9021b72e7fe74cd3534de51218217f57803a2db7
AppNetDeveloper <liviudiaconu@appnet.dev>
2024-05-13 13:51:53 +0200

    update

--- config/filesystems.php
+++ config/filesystems.php
@@ -70,7 +70,7 @@ return [
         'sftp' => [
             'driver' => 'sftp',
             'host' => env('SFTP_HOST'),
-            'port' => env('SFTP_PORT', 22),
+            'port' => 22,
             'username' => env('SFTP_USERNAME'),
             'password' => env('SFTP_PASSWORD'),
             'root' => env('SFTP_ROOT'),
