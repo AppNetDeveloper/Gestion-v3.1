434644c648a5ce084722841bdf41f1d5b192568f
AppNetDeveloper <liviudiaconu@appnet.dev>
2025-03-23 20:49:09 +0100

    main

--- app/Http/Controllers/AutoProcessController.php
+++ app/Http/Controllers/AutoProcessController.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\Auth;
 
 class AutoProcessController extends Controller
 {
-    public function update(Request $request)
+    public function updateWhatsapp(Request $request)
     {
         // Validación: se requiere que 'whatsapp' tenga un valor de 0,1,2 o 3 y si es mayor que 0, 'whatsapp_prompt' es obligatorio.
         $data = $request->validate([
@@ -29,4 +29,37 @@ class AutoProcessController extends Controller
             'message' => 'Configuración actualizada correctamente.'
         ]);
     }
+    public function updateTelegram(Request $request)
+    {
+        // Validación: se requiere que 'whatsapp' tenga un valor de 0,1,2 o 3 y si es mayor que 0, 'whatsapp_prompt' es obligatorio.
+        $data = $request->validate([
+            'telegram'        => 'required|in:0,1,2,3',
+            'telegram_prompt' => 'required_if:telegram,1,2,3'
+        ]);
+
+        $user = Auth::user();
+
+        // Actualiza o crea el registro de configuración para el usuario
+        AutoProcess::updateOrCreate(
+            ['user_id' => $user->id],
+            $data
+        );
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Configuración actualizada correctamente.'
+        ]);
+    }
+
+    public function getTelegram(Request $request)
+    {
+        $user = Auth::user();
+        $autoProcess = AutoProcess::where('user_id', $user->id)->first();
+
+        return response()->json([
+            'success' => true,
+            'data'    => $autoProcess
+        ]);
+    }
+
 }
